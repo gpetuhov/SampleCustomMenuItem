e22706a986157ff691ee6fd5ffaff865022a2b76
Gennady Petukhov <gpetukhov@phoenixit.ru>
2018-05-18 10:29:09 +0300

    Add reload button

--- app/src/main/java/com/gpetuhov/android/samplecustommenuitem/MainActivity.java
+++ app/src/main/java/com/gpetuhov/android/samplecustommenuitem/MainActivity.java
@@ -2,6 +2,9 @@ package com.gpetuhov.android.samplecustommenuitem;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,24 @@ public class MainActivity extends AppCompatActivity {
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+  }
+
+  @Override
+  public boolean onCreateOptionsMenu(Menu menu) {
+    super.onCreateOptionsMenu(menu);
+    getMenuInflater().inflate(R.menu.activity_main_menu, menu);
+    return true;
+  }
+
+  @Override
+  public boolean onOptionsItemSelected(MenuItem item) {
+    switch (item.getItemId()) {
+      case R.id.activity_main_update_menu_item:
+        Toast.makeText(this, "update clicked", Toast.LENGTH_SHORT).show();
+        return true;
+
+      default:
+        return super.onOptionsItemSelected(item);
+    }
   }
 }
