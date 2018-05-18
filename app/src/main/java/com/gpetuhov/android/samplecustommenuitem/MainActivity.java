99fd89522d387e3ee952b334f6bee41e08b77227
Gennady Petukhov <gpetukhov@phoenixit.ru>
2018-05-18 10:38:30 +0300

    Set reload button click listener

--- app/src/main/java/com/gpetuhov/android/samplecustommenuitem/MainActivity.java
+++ app/src/main/java/com/gpetuhov/android/samplecustommenuitem/MainActivity.java
@@ -4,6 +4,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -18,7 +20,23 @@ public class MainActivity extends AppCompatActivity {
   public boolean onCreateOptionsMenu(Menu menu) {
     super.onCreateOptionsMenu(menu);
     getMenuInflater().inflate(R.menu.activity_main_menu, menu);
+    initReloadButton(menu);
     return true;
+  }
+
+  private void initReloadButton(Menu menu) {
+    final MenuItem reloadMenuItem = menu.findItem(R.id.activity_main_update_menu_item);
+
+    if (reloadMenuItem != null) {
+      View reloadActionView = reloadMenuItem.getActionView();
+
+      if (reloadActionView != null) {
+        ImageView reloadButton = reloadActionView.findViewById(R.id.reload_button);
+        if (reloadButton != null) {
+          reloadButton.setOnClickListener(v -> onOptionsItemSelected(reloadMenuItem));
+        }
+      }
+    }
   }
 
   @Override
